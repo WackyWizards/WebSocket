8160ca484d4dcd9310102d8ae4655f39cdfa59d5
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-03-08 21:27:21 +0200

    Add RequestMessage.CreateAsync

--- Code/Schema/RequestMessage.cs
+++ Code/Schema/RequestMessage.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace WebSocket.Schema;
 
 public class RequestMessage : Message
 {
-	public RequestMessage( string type, string content = null )
+	private RequestMessage( string type, string steamId, string content, string token )
 	{
 		Type = type;
+		SteamId = steamId;
 		Content = content;
 		CorrelationId = Guid.NewGuid().ToString();
+		Token = token;
+	}
+
+	public static async Task<RequestMessage> CreateAsync( string type, string steamId, string content )
+	{
+		var token = await Sandbox.Services.Auth.GetToken( WebSocketConnection.Instance.ServiceName );
+		return new RequestMessage( type, steamId, content, token );
 	}
 }
