b3787f49ae95b57add7f305bb00e94bdeb8cc03c
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-03-08 21:27:08 +0200

    Add Message.SteamId, Message.Token

--- Code/Schema/Message.cs
+++ Code/Schema/Message.cs
@@ -7,6 +7,12 @@ public class Message
 {
 	[JsonPropertyName( "type" )]
 	public string Type { get; set; }
+	
+	[JsonPropertyName( "steamId" )]
+	public string SteamId { get; set; }
+	
+	[JsonPropertyName( "token" )]
+	public string Token { get; set; }
 
 	[JsonPropertyName( "content" )]
 	public string Content { get; set; }
