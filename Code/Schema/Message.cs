2605fd480fa3f2d38fcc363e5ed13861a50180ec
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-03-27 22:15:50 +0200

    This should probably be internal

--- Code/Schema/Message.cs
+++ Code/Schema/Message.cs
@@ -18,7 +18,7 @@ public class Message
 	public string Content { get; set; }
 	
 	[JsonPropertyName("correlationId")]
-	public string CorrelationId { get; set; }
+	internal string CorrelationId { get; set; }
 
 	public static implicit operator Message( string json )
 	{
